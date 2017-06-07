fa5a762d1913ad4636c9fd443fab8f1c15fff0d1
kade042 <dereje88@gmail.com>
2017-06-07 15:08:38 -0600

    setup index.js and App.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { render }from 'react-dom';
 import App from './App';
-import Board from './components/Board';
-import Square from './components/Square';
-import Knight from './components/Knight';
-import { observe } from './components/Game';
+
+var data = [
+    {
+     'when': '2 minutes ago',
+     'who': 'Jill Dupre',
+     'description': 'Created new account',
+    },
+    {
+     'when': '1 hours ago',
+     'who': 'Lose White',
+     'description': 'Added fist chapter',
+    },
+    {
+     'when': '2 hours ago',
+     'who': 'Jordan Whash',
+     'description': 'Created new account',
+   },
+ ];
+
+var headings = ['Last updated on', 'By Author', 'Summary'];
+var title = 'Recent changes';
+
 
 const rootEl = document.getElementById('root');
 
-observe(knightPosition =>
-  render(
-    <Board knightPosition={ knightPosition } />,
+render (
+    <App title={title} />,
     rootEl
-  )
-);
+  );
